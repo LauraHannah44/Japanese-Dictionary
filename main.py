5a887c868ab7e4669968bc9f13c288d89cbdc597
Laura <laurahannah44@gmail.com>
2020-07-21 02:01:21 +0100

    Implimented reading from Word Storage.

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ class Kanji:
     @classmethod
     def define_kanji_list(cls):
         data = pandas.read_excel("Kanji Storage.xlsx")
-        for kanji_index in range(228):
+        for kanji_index in range(len(data.index)):
             row = data.loc[kanji_index, :]
 
             writing = row.loc["writing"]
@@ -73,10 +73,10 @@ class Kanji:
                     self.readings[yomi].append(reading)
 
     def __repr__(self):
-        return "K(" + self.writing + ")"
+        return "Kanji(" + self.writing + ")"
 
     def __str__(self):
-        summary = self.writing + " meaning " + self.translation
+        summary = "kanji " + self.writing + " meaning “" + self.translation + "”"
         for yomi in self.readings:
             summary += "\nread as " + yomi
             if len(self.readings[yomi]) > 0:
@@ -100,23 +100,34 @@ class Kanji:
 
 
 class Word:
+    words      = dict()
+    nouns      = dict()
+    pronouns   = dict()
+    verbs      = dict()
+    adjectives = dict()
+    adverbs    = dict()
+
     def __init__(self, translation, kanji, word_type,
         category=None,
         t_rating=0, r_rating=0,
+        t_similarity=None, r_similarity=None,
         in_mind_map=False, notes=None
     ):
 
         self.translation = translation
         self.kanji = kanji
 
-        self.word_type = word_type
-        self.category = category
+        self.word_type = (word_type, None)[word_type == "nan"]
+        self.category = (category, None)[category == "nan"]
 
-        self.t_rating = t_rating  # translation
-        self.r_rating = r_rating  # reading
+        self.t_rating = int((t_rating, 0)[math.isnan(t_rating)])  # translation
+        self.r_rating = int((r_rating, 0)[math.isnan(r_rating)])  # reading
 
-        self.in_mind_map = in_mind_map
-        self.notes = notes
+        self.t_similarity = (t_similarity, None)[t_similarity == "nan"]  # translation
+        self.r_similarity = (r_similarity, None)[r_similarity == "nan"]  # reading
+
+        self.in_mind_map = bool((in_mind_map, False)[in_mind_map == "nan"])
+        self.notes = (notes, None)[notes == "nan"]
 
         self.writing = str()
         self.reading = str()
@@ -129,12 +140,45 @@ class Word:
                 self.writing += reading_kanji
                 self.reading += reading_kanji
 
+    @classmethod
+    def define_word_list(cls):
+        data = pandas.read_excel("Word Storage.xlsx")
+        for kanji_index in range(len(data.index)):
+            row = data.loc[kanji_index, :]
+
+            translation = row.loc["translation"]
+
+            kanji = list()
+            for reading, writing in zip(row.loc["reading"].split(", "), list(row.loc["writing"])):
+                if reading == writing:
+                    kanji.append(reading)
+                else:
+                    kanji.append((reading, Kanji.kanji[writing]))
+            kanji = tuple(kanji)
+
+            word_type = str(row.loc["word_type"])
+
+            category = str(row.loc["category"])
+
+            t_rating = row.loc["t_rating"]
+            r_rating = row.loc["r_rating"]
+
+            t_similarity = str(row.loc["t_similarity"])
+            r_similarity = str(row.loc["r_similarity"])
+
+            in_mind_map = str(row.loc["in_mind_map"])
+            notes = str(row.loc["notes"])
+
+            cls.words[row.loc["writing"]] = cls(translation, kanji, word_type, category,
+                                     t_rating, r_rating, t_similarity, r_similarity,
+                                     in_mind_map, notes)
+
     def __repr__(self):
-        return "W(" + self.writing + ")"
+        return "Word(" + self.writing + ")"
 
     def __str__(self):
-        summary = self.word_type + " " + self.writing + " read as " + self.reading
-        summary += "\nmeaning " + self.translation
+        summary = self.word_type + " " + self.writing + " read as " + self.reading + ""
+        summary += "\nmeaning “" + self.translation + "”"
         summary += "\nfrom "
         for kanji in self.kanji:
             if isinstance(kanji, str):
@@ -152,17 +196,13 @@ class Word:
 # generally want to learn to translate writing -> translation -> reading
 
 Kanji.define_kanji_list()
-#print(Kanji.kanji)
+print(Kanji.kanji)
+kanji = Kanji.kanji
 
-words = list()
-#words.append(Word("", (), ""))
-words.append(Word("university", (("ダイ", Kanji.kanji["大"]), ("ガク", Kanji.kanji["学"])), "noun", category="learning", t_rating=5, r_rating=5, in_mind_map=True))
-words.append(Word("university student", (("ダイ", Kanji.kanji["大"]), ("ガク", Kanji.kanji["学"]), ("セイ", Kanji.kanji["生"])), "noun", category="learning", t_rating=5, r_rating=5, in_mind_map=True))
-words.append(Word("student", (("ガク", Kanji.kanji["学"]), ("セイ", Kanji.kanji["生"])), "noun", category="learning", t_rating=5, r_rating=5, in_mind_map=True))
-words.append(Word("to study", (("まな", Kanji.kanji["学"]), "ぶ"), "verb", category="learning", t_rating=4, r_rating=3, in_mind_map=True))
-words.append(Word("much", (("おお", Kanji.kanji["大"]), "いに"), "adverb", t_rating=4, r_rating=5, in_mind_map=True))
-words.append(Word("big", (("おお", Kanji.kanji["大"]), "きい"), "adjective", t_rating=5, r_rating=5, in_mind_map=True))
+Word.define_word_list()
+print(Word.words)
+word = Word.words
 
-print(str(Kanji.kanji["後"]))
+print(word["学ぶ"])
 print()
-print(str(words[3]))
+print(word["学生"])
